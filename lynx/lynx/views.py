3c68cef7b8f2e9c664cf31d00e6ddf9cbafd1e30
mjtolentino <Nili2bf*GL>
2020-06-21 14:52:24 -0700

    making changes for shane

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -413,8 +413,8 @@ class ProgressReportDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(ProgressReportDetailView, self).get_context_data(**kwargs)
-        MONTHS = (("January", 1), ("February", 2), ("March", 3), ("April", 4), ("May", 5), ("June", 6), ("July", 7),
-                  ("August", 8), ("September", 9), ("October", 10), ("November", 11), ("December", 12))
+        MONTHS = {("January", 1), ("February", 2), ("March", 3), ("April", 4), ("May", 5), ("June", 6), ("July", 7),
+                  ("August", 8), ("September", 9), ("October", 10), ("November", 11), ("December", 12)}
 
         # print(context)
         report = ProgressReport.objects.filter(id=self.kwargs['pk']).values()
