113744550799bde9c791fcdbdf6dba07b3302c43
mjtolentino <Nili2bf*GL>
2020-07-26 19:42:19 -0700

    making changes for priscilla - bulk add sip note.

--- lynx/mysite/settings.py
+++ lynx/mysite/settings.py
@@ -41,12 +41,10 @@ INSTALLED_APPS = [
     'crispy_forms',
     'mathfilters',
     'openpyxl',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
