bc3b6c2adaaede61721092b1f2e034967dc9f7e7
mjtolentino <Nili2bf*GL>
2020-07-26 18:40:39 -0700

    making changes for priscilla - bulk add sip note.

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -104,7 +104,7 @@ def add_sip_note_bulk(request):
     form = SipNoteForm()
     if request.method == 'POST':
         form = SipNoteForm(request.POST)
-        for client in form.cleaned_data.get('multiselect'):
+        for client in form.get('multiselect'):
             if form.is_valid():
                 form = form.save(commit=False)
                 form.pk = None
