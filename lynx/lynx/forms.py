891116d8f0da00c94d3513ab7264bab39c084e34
mjtolentino <Nili2bf*GL>
2020-12-13 10:41:00 -0800

    adding hour validator -adding auth id before validation

--- lynx/lynx/forms.py
+++ lynx/lynx/forms.py
@@ -169,7 +169,7 @@ class LessonNoteForm(forms.ModelForm):
         data = self.cleaned_data['billed_units']
 
         from .views import units_to_hours
-        authorization_id = self.cleaned_data['authorization_id']
+        authorization_id = self.cleaned_data['authorization']
         note_list = LessonNote.objects.filter(authorization_id=authorization_id)
         authorization = Authorization.objects.get(id=authorization_id)
 
