0ce7d9f8f6cf4c0509b65f3877bbd80d92eb4452
mjtolentino <Nili2bf*GL>
2020-12-13 20:47:58 -0800

    fixing plan name

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -124,7 +124,7 @@ def add_sip_plan(request, contact_id):
         form = SipPlanForm(request.POST)
         if form.is_valid():
             form = form.save(commit=False)
-            form.plan_name = request.POST['start_date'] + ' - ' + request.POST['plan_type'] + ' - ' + request.POST['instructor']
+            form.plan_name = request.POST.get('start_date') + ' - ' + request.POST.get('plan_type') + ' - ' + request.POST.get('instructor')
             form.contact_id = contact_id
             form.user_id = request.user.id
             form.save()
