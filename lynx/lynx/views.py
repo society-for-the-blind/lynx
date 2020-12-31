85c1d100cae28109c1cb24b6dda35b9b699e4a1f
mjtolentino <Nili2bf*GL>
2020-12-31 15:26:03 -0800

    fix sip note plans

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -94,7 +94,7 @@ def add_intake(request, contact_id):
 
 @login_required
 def add_sip_note(request, contact_id):
-    form = SipNoteForm(None, contact_id=contact_id)
+    form = SipNoteForm(request, contact_id=contact_id)
     if request.method == 'POST':
         form = SipNoteForm(request.POST, contact_id=contact_id)
         if form.is_valid():
