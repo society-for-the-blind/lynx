011238803e150288638f91aaf94bced5f926f8c3
mjtolentino <Nili2bf*GL>
2021-01-23 16:43:04 -0800

    edit sip note needs to update the quarter and fiscal year

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -835,12 +835,12 @@ class SipNoteUpdateView(LoginRequiredMixin, UpdateView):
     #     return redirect_url
 
     def post(self, request, **kwargs):
+        self.object = self.get_object()
         request.POST = request.POST.copy()
         note_date = request.POST['note_date']
         note_date = datetime.strptime(note_date, '%Y-%m-%d')
         note_month = note_date.month
         note_year = note_date.year
-
         quarter = get_quarter(note_month)
         if quarter == 1:
             fiscal_year = get_fiscal_year(note_year)
