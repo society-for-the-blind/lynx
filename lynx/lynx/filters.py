f5dca81c9af72ea1d666f7edce55eceeb667b9c4
mjtolentino <Nili2bf*GL>
2021-02-07 12:10:14 -0800

    search dropdown items

--- lynx/lynx/filters.py
+++ lynx/lynx/filters.py
@@ -1,8 +1,12 @@
 import django_filters
 
-from .models import ContactInfoView
+from .models import ContactInfoView, AGES, COUNTIES
+
 
 class ContactFilter(django_filters.FilterSet):
+    age_group = django_filters.ChoiceFilter(choices=AGES)
+    county = django_filters.ChoiceFilter(choices=COUNTIES)
+
     class Meta:
         model = ContactInfoView
         fields = {'last_name': ['icontains'],
