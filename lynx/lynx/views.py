196b219a82088026df6e9816f440a2ce813e99d4
mjtolentino <Nili2bf*GL>
2021-02-20 14:23:59 -0800

    trying to condense client search result

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -1609,7 +1609,7 @@ def contact_list(request):
         strict = True
         f = ContactFilter(request.GET, queryset=ContactInfoView.objects.all().order_by(Lower('full_name')))
 
-        client_condensed = []
+        client_condensed = {}
         for client in f.qs:
             if client.id in client_condensed:
                 client_condensed[client.id]['phone_number'] = client_condensed[client.id]['phone_number'] + ', ' + client.phone_number
