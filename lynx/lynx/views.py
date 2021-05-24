300ef7ebf52606e995734eed78b414903af82592
mjtolentino <Nili2bf*GL>
2021-05-23 19:11:12 -0700

    add volunteer report

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -502,7 +502,7 @@ def volunteers_report_month(request):
                 MONTHS = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July",
                           8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
                 given_month = MONTHS[vol.month]
-                date = given_month + ' ' + vol.year
+                date = given_month + ' ' + str(vol.year)
                 hours = vol.hours
                 writer.writerow([name, date, hours])
 
