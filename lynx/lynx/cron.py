88dfecff16ccc5e904d338813605b0ba43dbfe82
mjtolentino <Nili2bf*GL>
2021-07-14 20:32:47 -0700

    update reports to match OIB report

--- lynx/lynx/cron.py
+++ lynx/lynx/cron.py
@@ -7,7 +7,7 @@ from django.template.loader import get_template
 from datetime import datetime, timedelta
 import os
 import sys
-from .cron_scripts import address
+from cron_scripts import address
 
 sys.path.append("/var/www/lynx/slate-2/lynx/")
 sys.path.append("/var/www/lynx/slate-2/lynx/mysite")
