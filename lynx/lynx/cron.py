76a8ee67fa87a1ab84db92b77f5b4934d815df99
mjtolentino <Nili2bf*GL>
2021-07-15 08:08:58 -0700

    fixing multiple email issue. Was sending 3x when I restarted the server. The issue was there are three workers in gunicorn, each of those where opening and running through my cron.py.

--- lynx/lynx/cron.py
+++ lynx/lynx/cron.py
@@ -7,7 +7,7 @@ from django.template.loader import get_template
 from datetime import datetime, timedelta
 import os
 import sys
-from cron_scripts import address
+#from cron_scripts import address
 
 sys.path.append("/var/www/lynx/slate-2/lynx/")
 sys.path.append("/var/www/lynx/slate-2/lynx/mysite")
@@ -15,4 +15,4 @@ sys.path.append("/var/www/lynx/slate-2/lynx/mysite")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 django.setup()
-address()
+#address()
