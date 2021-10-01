bd6e690f6a94ae14b420d4107b01d9ee792cd657
mjtolentino <Nili2bf*GL>
2021-09-30 17:37:14 -0700

    allowing more formats for input date

--- lynx/mysite/urls.py
+++ lynx/mysite/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path("lynx/", include('lynx.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    url(r"^account/", include("account.urls")),
 ]
 
 if settings.DEBUG:
