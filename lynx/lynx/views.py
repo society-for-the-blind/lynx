fc8ad94ecddd5a7df127cd670972a7b23094d5cb
mjtolentino <Nili2bf*GL>
2021-10-13 11:57:37 -0700

    fixing payment list on auth and intake edit

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -870,9 +870,9 @@ class BillingReviewDetailView(LoginRequiredMixin, DetailView):
 
         context['note_list'] = notes
 
-        agency_id = authorization[0]['outside_agency_id']
-        outside = OutsideAgency.objects.filter(id=agency_id).values()
-        context['payment'] = outside[0]['contact_name'] + ' - ' + outside[0]['agency']
+        contact_id = authorization[0]['outside_agency_id']
+        outside = Contact.objects.filter(id=contact_id).values()
+        context['payment'] = outside[0]['first_name'] + ' ' + outside[0]['last_name'] + ' - ' + outside[0]['company']
         contact_id = outside[0]['contact_id']
         address = Address.objects.filter(contact_id=contact_id).values()[:1]
         context['address'] = address
