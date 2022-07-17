9bbc1b6b1415f7aca3f4589979e7d34fb877a8c8
mjtolentino <Nili2bf*GL>
2022-07-17 09:52:37 -0700

    delete link and fixed redirect

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -417,7 +417,7 @@ def add_authorization(request, contact_id):
             form.user_id = request.user.id
             form.active = 1
             form.save()
-            return HttpResponseRedirect(reverse('lynx:client', args=(contact_id,)))
+            return HttpResponseRedirect(reverse('lynx:authorization_detail', args=(contact_id,)))
     return render(request, 'lynx/add_authorization.html', {'form': form})
 
 
