495fabfaf4e33161e800b458481cb185f2fa1fea
Attila Gulyas <toraritte@gmail.com>
2024-04-16 00:53:21 +0000

    Add assignment row ids [..]
    See  more in  the notes  in `instructor_search.html`
    template.

--- lynx/lynx/views.py
+++ lynx/lynx/views.py
@@ -2510,6 +2510,7 @@ def assignment_advanced_result_view(request):
         assignment_condensed = {}
         for assignment in f.qs:
             assignment_condensed[assignment.id] = {}
+            assignment_condensed[assignment.id]['assignment_id'] = assignment.id if assignment.id is not None else ''
             assignment_condensed[assignment.id]['assignment_date'] = assignment.assignment_date if assignment.assignment_date is not None else ''
             assignment_condensed[assignment.id]['client_id'] = assignment.contact_id if assignment.contact_id is not None else ''
             assignment_condensed[assignment.id]['client_first_name'] = assignment.contact.first_name if assignment.contact.first_name is not None else ''
